db97664fd14e08d9cc4e3c0607e9ec1847f3809e
Karl Southern <karl@theangryangel.co.uk>
2015-01-27 23:01:59 +0000

    SMU

--- bin/time-left-in-the-hell-hole.rb
+++ bin/time-left-in-the-hell-hole.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
 
 require 'date'
-puts ((DateTime.parse('2014-07-06')-DateTime.now()).to_f/7).round(1).to_s + ' weeks remaining'
+puts ((DateTime.parse('2015-07-01')-DateTime.now()).to_f/7).round(1).to_s + ' weeks remaining'
