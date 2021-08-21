f7c405bb7e93214ca1a7bde5e088022a003a57f6
Karl Southern <karl@theangryangel.co.uk>
2021-08-21 18:22:39 +0100

    feat: play with chezmoi

--- bin/arch-cleanup.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# remove unused packages - check carefully
-sudo pacman -Rsn $(pacman -Qdtq)
-
-# pacman cache clean up
-sudo pacman -Sc
-
-sudo pacman-optimize && sudo sync
--- bin/time-left-in-the-hell-hole.rb
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env ruby
-
-require 'date'
-puts ((DateTime.parse('2015-07-01')-DateTime.now()).to_f/7).round(1).to_s + ' weeks remaining'
